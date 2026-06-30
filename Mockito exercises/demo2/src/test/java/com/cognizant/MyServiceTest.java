991bd14653cf979011c0d8ef399f38335c88c6d1
Sambari Jyothsna <Jyothsnas809@gmail.com>
2026-06-30 22:05:54 +0530

    Add files via upload

---
+++ Mockito exercises/demo2/src/test/java/com/cognizant/MyServiceTest.java
@@ -0,0 +1,20 @@
+package com.cognizant;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import static org.mockito.Mockito.verify;
+
+public class MyServiceTest {
+
+    @Test
+    public void testVerifyInteraction() {
+
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+
+        System.out.println("Interaction verified successfully.");
+
+    }
+}
