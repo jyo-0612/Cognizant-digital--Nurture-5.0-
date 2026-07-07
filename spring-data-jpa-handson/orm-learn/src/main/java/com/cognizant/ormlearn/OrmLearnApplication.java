000da9d07fb0e5e7e1cc6102360d4d80f5725e70
Sambari Jyothsna <Jyothsnas809@gmail.com>
2026-07-07 19:00:17 +0530

    Add files via upload

---
+++ spring-data-jpa-handson/orm-learn/src/main/java/com/cognizant/ormlearn/OrmLearnApplication.java
@@ -0,0 +1,31 @@
+package com.cognizant.ormlearn;
+
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import com.cognizant.ormlearn.model.Country;
+import com.cognizant.ormlearn.service.CountryService;
+
+@SpringBootApplication
+public class OrmLearnApplication {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrmLearnApplication.class);
+
+    public static void main(String[] args) {
+        ApplicationContext context = SpringApplication.run(OrmLearnApplication.class, args);
+        LOGGER.info("Inside main");
+        
+        CountryService countryService = context.getBean(CountryService.class);
+        testGetAllCountries(countryService);
+    }
+
+    private static void testGetAllCountries(CountryService countryService) {
+        LOGGER.info("Start");
+        List<Country> countries = countryService.getAllCountries();
+        LOGGER.debug("countries={}", countries);
+        LOGGER.info("End");
+    }
+}
